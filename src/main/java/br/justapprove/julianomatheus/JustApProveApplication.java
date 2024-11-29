de80ea9e6536485941fd9d0307576a03dfbabc13
MatheusMazzuco <matheusflores.ss029@academico.ifsul.edu.br>
2024-11-28 22:30:58 -0300

    just cleaning up some comments and code

--- src/main/java/br/justapprove/julianomatheus/JustApProveApplication.java
+++ src/main/java/br/justapprove/julianomatheus/JustApProveApplication.java
@@ -13,29 +13,5 @@ public class JustApProveApplication {
 	    System.setProperty("EMAIL_PASSWORD", dotenv.get("EMAIL_PASSWORD"));
 		SpringApplication.run(JustApProveApplication.class, args);
 	}
-//Repository vs DAO: https://stackoverflow.com/questions/59797882/is-jparepository-interface-covers-the-responsibilities-of-dao-interface-in-sprin
 	
-	//usar service? : https://medium.com/@chandantechie/spring-boot-application-with-crud-operations-using-spring-data-jpa-and-mysql-23c8019660b1
-	
-	//possivel soluçao para os problemas de arquivo: https://www.youtube.com/watch?v=XUL60-Ke-L8
-
-/*possiveis soluções de json parser deserialization error:
-	https://devwithus.com/can-not-deserialize-instance-of-java-util-arraylist-start-object-token/
-	
-	https://stackoverflow.com/questions/10978748/jackson-deserialization-with-contained-arraylistt
-	
-	https://www.geeksforgeeks.org/how-to-convert-an-arraylist-of-objects-to-a-json-array-in-java/
-	
-	https://stackoverflow.com/questions/20837856/can-not-deserialize-instance-of-java-util-arraylist-out-of-start-object-token
-	
-	https://www.javatpoint.com/deserialize-to-collection-and-array-in-jackson
-	
-	https://stackoverflow.com/questions/9829403/deserialize-json-to-arraylistpojo-using-jackson
-	
-	https://www.youtube.com/watch?v=3ONGz-eSWlg&t=79s
-	
-	https://pt.stackoverflow.com/questions/375677/como-corrigir-o-erro-cannot-deserialize-from-object-value-no-delegate-or-prop
-	
-	SIM, EU PROCUREI EM TUDO ISSO E NÃO ACHEI A SOLUÇÃO ;-;
-	*/
 }
--- src/main/java/br/justapprove/julianomatheus/service/EmailService.java
+++ src/main/java/br/justapprove/julianomatheus/service/EmailService.java
@@ -11,8 +11,8 @@ import jakarta.mail.internet.MimeMessage;
 @Service
 public class EmailService {
 	 @Autowired
-	    private JavaMailSender mailSender;
-	public void enviarEmail(String para, String assunto, String texto) {
+	 private JavaMailSender mailSender;
+	 public void enviarEmail(String para, String assunto, String texto) {
         try {
             // Cria a mensagem MIME
             MimeMessage mensagem = mailSender.createMimeMessage();
@@ -24,7 +24,7 @@ public class EmailService {
             helper.setText(texto, true); // `true` para HTML, `false` para texto simples
             
             // Configura o remetente com InternetAddress
-         helper.setFrom(new InternetAddress("no-reply@justapprove.com", "JustAPProve"));
+            helper.setFrom(new InternetAddress("no-reply@justapprove.com", "JustAPProve"));
 
             // Envia o e-mail
             mailSender.send(mensagem);
